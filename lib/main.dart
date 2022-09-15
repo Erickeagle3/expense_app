b2189531f06995e6a208162187d52e6238dbaa8d
Erickeagle3 <sayanchakraborty334@gmail.com>
2022-09-15 16:17:34 +0530

    flutter: added time stamp, title and price to the card from map Signed-off-by: Erickeagle3 <sayanchakraborty334@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'trans.dart';
 
@@ -53,8 +55,20 @@ class MyApp extends StatelessWidget {
             Column(
               children: trans.map((exp) {
                 return Card(
-                  child: Text(exp.id),
-                );
+                    child: Row(
+                  children: <Widget>[
+                    // ignore: avoid_unnecessary_containers
+                    Container(
+                      child: Text(exp.amount.toString()),
+                    ),
+                    Column(
+                      children: <Widget>[
+                        Text(exp.title),
+                        Text(exp.date.toString()),
+                      ],
+                    )
+                  ],
+                ));
               }).toList(),
             )
           ],
