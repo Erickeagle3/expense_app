02dc690c96c7ad6b5cba9e454dc2422e6d81eed7
Erickeagle3 <sayanchakraborty334@gmail.com>
2022-09-15 14:56:41 +0530

    flutter: initial stage of card widget Signed-off-by: Erickeagle3 <sayanchakraborty334@gmail.com>
    Note - changing spaces and line's are just a part of formating code whice make our code more beatiful

--- lib/main.dart
+++ lib/main.dart
@@ -12,18 +12,27 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Expenses Calculator'),
-          titleTextStyle: const TextStyle(
-              fontSize: 18, fontWeight: FontWeight.bold, color: Colors.black),
-       centerTitle: true,
-       toolbarOpacity: 0.9,
-       shape: const RoundedRectangleBorder(
-        borderRadius: BorderRadius.only(
-         bottomLeft: Radius.circular(35),
-         bottomRight: Radius.circular(35)
-        )),
-        backgroundColor: Colors.red.shade300),
+            title: const Text('Expenses Calculator'),
+            titleTextStyle: const TextStyle(
+                fontSize: 18, fontWeight: FontWeight.bold, color: Colors.black),
+            centerTitle: true,
+            toolbarOpacity: 0.9,
+            shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(35),
+                    bottomRight: Radius.circular(35))),
+            backgroundColor: Colors.red.shade300),
+        body: Column(
+          children: const <Widget>[
+            Card(
+              child: Text('chart overview'),
+            ),
+            Card(
+              child: Text('list of expenses'),
+            )
+          ],
         ),
-      );
+      ),
+    );
   }
 }
